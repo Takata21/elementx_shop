15d84530b034d4a2c596d97d638f07f0099cba5d
TAKATA <takata211011@mail.com>
2022-06-12 17:49:35 -0500

    login user done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/provider/AuthContext';
 
 const Login = () => {
+  let navigate = useNavigate();
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+  const { signin, isLoading, errorMessage } = useAuth();
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const userResponse = await signin(user);
+      if (userResponse) {
+        navigate('/');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="register">
       <Link to="/">
@@ -17,16 +39,28 @@ const Login = () => {
         />
       </Link>
       <div className="register-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h4>Iniciar Sesion</h4>
 
           <div className="form-control-container">
-            <label htmlFor="">correo electronico</label>
-            <input type="email" />
+            <label htmlFor="email">correo electronico</label>
+            <input
+              onChange={handleChange}
+              id="email"
+              name="email"
+              type="email"
+              required
+            />
           </div>
           <div className="form-control-container">
-            <label htmlFor="">contraseña</label>
-            <input type="password" />
+            <label htmlFor="password">contraseña</label>
+            <input
+              onChange={handleChange}
+              id="password"
+              name="password"
+              type="password"
+              required
+            />
           </div>
 
           <button className="register-btn" type="submit">
